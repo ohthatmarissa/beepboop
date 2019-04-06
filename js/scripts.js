67211b5fafd43d7b993f5c75f1b2eb7bcc02a0d5
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-06 16:25:52 -0700

    add JS loop and if/else that should work but doesn't

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
 
 function modify(userInput) {
 
- if (isNaN(userInput)){
+ if (isNaN(userInput))
     return 'Please enter a valid number';
-  } else {
-    return userInput;
+  else
+    return userInput
 
-  }
+  if (!isNaN(userInput))
+  var text = "";
+  var i;
+  for (i = 0; i < 10; i++)
+    text += "" + i + "<br>";
+
+
+    if (userInput[0-10] === '1')
+      return 'Beep'
+    else if (userInput[1-10] === '2')
+      return 'Boop'
+    else (userInput[1-10] === '3')
+      return 'Im sorry Dave. Im afraid I cant do that'
 }
 
 $(document).ready(function() {
@@ -16,7 +28,6 @@ $(document).ready(function() {
   var userInput = $('input#number').val();
   var result = modify(userInput);
   $('#result').text(result);
-
 
   });
 });
