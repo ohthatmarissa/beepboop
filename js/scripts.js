93295d49404ab03cf522e84ff460d1fbb64b4332
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-07 15:00:56 -0700

    add more js logic if/else, array

--- js/scripts.js
+++ js/scripts.js
@@ -17,19 +17,24 @@ $(document).ready(function() {
     //   return 'Please enter a valid number';
     // else
   // var oneTwoThree = [1, 2, 3];
+    var array = [];
+    for (var i = 0; i <= userInput; i++){
+      array.push(userInput);
 
-    for (var i = 0; i <= userInput; i++)
-      // console.log(i)
-      if ((i+'').indexOf('3') > -1);
-      console.log('im sorry Dave.');
-      // else if ((i+'').indexOf('2') > -1);
-      // console.log('boop');
-      // else if ((i+'').indexOf('1') > -1);
-      // console.log('beep');
-      // else
-      // console.log(i);
+      console.log(i)
+      if ((i+'').indexOf('3') > -1)
+        console.log('im sorry Dave.');
+      else if ((i+'').indexOf('2') > -1)
+        console.log('boop');
+      else if ((i+'').indexOf('1') > -1)
+        console.log('beep');
+      else
+        console.log(i);
+
+
+
     }
-
+  }
 
 
   });
