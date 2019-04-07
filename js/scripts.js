7b45a3c76e5b6d5e46e11d9ea616ad0c2f56bf67
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-07 16:14:30 -0700

    add append to string conditional

--- js/scripts.js
+++ js/scripts.js
@@ -13,27 +13,43 @@ $(document).ready(function() {
 
 
   function modify(userInput) {
-    // if (isNaN(userInput))
-    //   return 'Please enter a valid number';
-    // else
+    if (isNaN(userInput))
+      return 'Please enter a valid number';
+    else
   // var oneTwoThree = [1, 2, 3];
-    var array = [];
-    for (var i = 0; i <= userInput; i++){
-      array.push(userInput);
+    var resultString = '';
+    for (var i = 0; i <= userInput; i++) {
 
-      console.log(i)
-      if ((i+'').indexOf('3') > -1)
-        console.log('im sorry Dave.');
-      else if ((i+'').indexOf('2') > -1)
-        console.log('boop');
-      else if ((i+'').indexOf('1') > -1)
-        console.log('beep');
-      else
-        console.log(i);
+      if (i.toString().indexOf('3') > -1) {
+        resultString = resultString + "i'm sorry Dave, ";
+      }
+      else if (i.toString().indexOf('2') > -1) {
+        resultString = resultString + "Boop, ";
+      }
+      else if (i.toString().indexOf('1') > -1) {
+        resultString = resultString + "Beep, ";
+      }
+      else {
+        resultString = resultString + i.toString() + ", ";
+      }
 
+      // if (i.toString().indexOf('3') > -1)
+      //   resultString = "im sorry Dave., ";
+      //   console.log('im sorry Dave.');
+      // else if ((i.toString()).indexOf('2') > -1)
+      //   resultString = resultString +'boop, ';
+      //   console.log('boop');
+      // else if ((i.toString()).indexOf('1') > -1)
+      //   resultString+'beep, ';
+      //   console.log('beep');
+      // else
+      //   resultString+i.toString();
+      //   console.log(i);
 
 
     }
+
+    return resultString;
   }
 
 
