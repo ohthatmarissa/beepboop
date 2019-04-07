1db0157fb13963166a334780abfda81475aef2a3
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-07 13:08:24 -0700

    add JS logic to for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,36 @@
 
-function modify(userInput) {
-
- if (isNaN(userInput))
-    return 'Please enter a valid number';
-  else
-    return userInput
-
-  if (!isNaN(userInput))
-  var text = "";
-  var i;
-  for (i = 0; i < 10; i++)
-    text += "" + i + "<br>";
 
 
-    if (userInput[0-10] === '1')
-      return 'Beep'
-    else if (userInput[1-10] === '2')
-      return 'Boop'
-    else (userInput[1-10] === '3')
-      return 'Im sorry Dave. Im afraid I cant do that'
-}
+
 
 $(document).ready(function() {
   $('form#beepBoop').submit(function(event) {
     event.preventDefault();
 
-  var userInput = $('input#number').val();
+  var userInput = parseInt($('input#number').val());
   var result = modify(userInput);
   $('#result').text(result);
+
+
+  function modify(userInput) {
+    // if (isNaN(userInput))
+    //   return 'Please enter a valid number';
+    // else
+  // var oneTwoThree = [1, 2, 3];
+
+    for (var i = 0; i <= userInput; i++)
+      // console.log(i)
+      if ((i+'').indexOf('3') > -1);
+      console.log('im sorry Dave.');
+      // else if ((i+'').indexOf('2') > -1);
+      // console.log('boop');
+      // else if ((i+'').indexOf('1') > -1);
+      // console.log('beep');
+      // else
+      // console.log(i);
+    }
+
+
 
   });
 });
