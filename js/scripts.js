9a6ff6070330b6670ebbe4c578e5606bff1fbd4a
Marissa Perry <ohthatmarissa@gmail.com>
2019-04-10 20:59:20 -0700

    fixed business and interface logic position. Added some sweet css

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
+//business logic-----------
+
+function modify(userInput) {
+  if (isNaN(userInput)) {
+    return 'Please enter a valid number';
+  }
+  else {
+    var resultArray = [];
+    for (var i = 0; i <= userInput; i++) {
+      var counterString = i.toString();
+      if (counterString.includes('3')) {
+        resultArray.push("Im sorry Dave");
+      }
+      else if (counterString.includes('2')) {
+        resultArray.push("Boop");
+      }
+      else if (counterString.includes('1')) {
+        resultArray.push("Beep");
+      }
+      else {
+        resultArray.push(counterString);
+      }
+    }
+    return resultArray.join(", ");
+  }
+}
+
+
+// user inerface logic -----------
+
 $(document).ready(function() {
   $('form#beepBoop').submit(function(event) {
     event.preventDefault();
@@ -5,30 +35,5 @@ $(document).ready(function() {
     var userInput = parseInt($('input#number').val());
     var result = modify(userInput);
     $('#result').text(result);
-
-    function modify(userInput) {
-      if (isNaN(userInput)) {
-        return 'Please enter a valid number';
-      }
-      else {
-        var resultArray = [];
-        for (var i = 0; i <= userInput; i++) {
-          var counterString = i.toString();
-          if (counterString.includes('3')) {
-            resultArray.push("Im sorry Dave");
-          }
-          else if (counterString.includes('2')) {
-            resultArray.push("Boop");
-          }
-          else if (counterString.includes('1')) {
-            resultArray.push("Beep");
-          }
-          else {
-            resultArray.push(counterString);
-          }
-        }
-        return resultArray.join(", ");
-      }
-    }
   });
 });
